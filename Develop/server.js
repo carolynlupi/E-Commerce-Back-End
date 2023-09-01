b4965ab7bc0a844e330d2f1e94d39ada1f926246
carolynlupi <karolynlupi@gmail.com>
2023-09-01 12:49:01 -0400

    Added to server.js in root folder

--- Develop/server.js
+++ Develop/server.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
-const sequelize = require('./config/connection');
+require('dotenv').config();
+
+// Import sequelize connection
+const Sequelize = require('sequelize');
+
+// Use environment variables for database connection details
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
+
+// Create a Sequelize instance with the environment variables
+const sequelize = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: 'mysql',
+  // ...other Sequelize options...
+});
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+// Sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
