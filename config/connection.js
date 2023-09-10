0a76c13561edeca8536bcb3a2523a7bd0b33f8db
carolynlupi <karolynlupi@gmail.com>
2023-09-10 03:39:02 -0400

    Day 900 same bug fix

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,10 @@ const Sequelize = require('sequelize');
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: '127.0.0.1',
+      host: 'localhost',
+      port: 8889,
       dialect: 'mysql',
+      database: process.env.DB_NAME,
       dialectOptions: {
         decimalNumbers: true,
       },
